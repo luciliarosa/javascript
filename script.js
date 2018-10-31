fb6711b58ff5c52404c6f2de56dcbede1baba9bb
luciliarosa <lucilia.rosa@fatec.sp.gov.br>
2018-10-30 21:57:04 -0300

    principios basicos de javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+// let nome = 'Lucilia Rosa';
+// let idade = 22;
+// let vivo = false;
+
+// let saida1 = 'Olá,' + nome + '! Tudo bem com você?';
+// saida1 = `Olá, ${nome}! Você tem ${idade} anos`;
+
+// console.log(saida1);
+// // alert('Hello World');
+
+// console.log(prompt('Qual o seu nome?'));
+// idade = prompt('Quantos anos você tem?');
+// console.log(idade);
+
+
+// vivo = confirm('Você está vivo?');
+// console.log(`Você está vivo? ${vivo}`);
+
+
+let number1 = Number(prompt('Digite número 1: '));
+let number2 = Number(prompt('Digite número 2: '));
+resultado = number1 + number2;
+
+alert(`${number1} + ${number2} = ${resultado}`);
+
+
